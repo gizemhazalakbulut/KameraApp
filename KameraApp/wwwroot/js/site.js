9821a3f620dad1b3a949833c502506e513c7eb48
gizemhazalakbulut <gizemhazalakbulut123@gmail.com>
2025-03-17 15:22:19 +0300

    Add project files.

---
+++ KameraApp/wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿
+document.getElementById('vertical-menu-btn').addEventListener('click', function () {
+    document.querySelector('.navbar-brand-box').classList.toggle('hidden-logo');
+    this.classList.toggle('menu-closed');
+});
